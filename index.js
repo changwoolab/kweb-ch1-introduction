781d239b549ee9d6cdbc10da76c4dc9c54ff34a1
CWY <cwyoo01@naver.com>
2021-09-14 21:23:31 +0900

    module

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const http = require("http");
+const template = require("./module")
 
 const hostname = "127.0.0.1";
 const port = 4000;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end(`Hello World\nI came back! Nodejs!`);
+    res.setHeader("Content-Type", "html");
+    res.end(template.html());
 });
 
 server.listen(port, hostname, () => {
---
+++ module.js
@@ -0,0 +1,17 @@
+module.exports = {
+    html: () => {
+        return `
+        <!doctype html>
+        <html>
+        <head>
+        <title>KWEB</title>
+        <meta charset="utf-8">
+        </head>
+        <body>
+        below is URL for index
+        <h1><a href="/">WEB</a></h1>
+        </body>
+        </html>
+        `
+    }
+}
